71e8837dcea36d5fc76823247deaf9b7e51bc10b
Tiago Gimenes <tlgimenes@gmail.com>
2022-11-16 17:10:01 -0300

    export default plugin

--- mod.ts
+++ mod.ts
@@ -4,22 +4,22 @@ import { partytownSnippet } from "npm:@builder.io/partytown@0.7.0/integration";
 
 import { storage } from "./shared.ts";
 
-export default function partytown(): Plugin {
-  const main = `data:application/javascript,export default function(state){
+const plugin: Plugin = {
+  name: "partytown",
+  entrypoints: {
+    "main": `data:application/javascript,export default function(state){
     window.forward = JSON.stringify(state);
-    ${partytownSnippet()}}`;
-    
-  return {
-    name: "partytown",
-    entrypoints: { "main": main },
-    render(ctx) {
-      storage.forward = [];
+    ${partytownSnippet()}}`,
+  },
+  render(ctx) {
+    storage.forward = [];
 
-      ctx.render();
+    ctx.render();
 
-      return {
-        scripts: [{ entrypoint: "main", state: storage }],
-      };
-    },
-  };
-}
+    return {
+      scripts: [{ entrypoint: "main", state: storage }],
+    };
+  },
+};
+
+export default plugin;
