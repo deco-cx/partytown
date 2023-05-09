9f6877648a5eee93cf4b550337e67ba8f7f1754c
gimenes <tlgimenes@gmail.com>
2023-05-09 11:02:27 -0300

    support GTM debug console

--- integrations/GTAG.tsx
+++ integrations/GTAG.tsx
@@ -1,38 +1,46 @@
-import { Head } from "$fresh/runtime.ts";
-
 import Script from "../Script.tsx";
 
 interface Props {
   trackingId: string;
 }
 
-const snippet = (trackingId: string) => `
-// It is safe to .push in datalayer in here because partytown have already
-// run and made dataLayer.push available in window
-window.gtag = window.gtag || function(){window.dataLayer.push(arguments)};
+declare global {
+  interface Window {
+    gtag: (...args: unknown[]) => void
+  }
+}
 
-function init() {
-  window.gtag("js", new Date());
-  window.gtag("config", "${trackingId}");
-};
+function snippet(trackingId: string) {
+  // It is safe to .push in datalayer in here because partytown have already
+  // run and made dataLayer.push available in window
+  window.gtag = window.gtag || function () {
+    window.dataLayer.push(arguments);
+  };
 
-if (document.readyState === 'complete') {
-  init();
-} else {
-  window.addEventListener('load', init);
-};
-`;
+  function init() {
+    window.gtag("js", new Date());
+    window.gtag("config", trackingId);
+  }
+
+  if (document.readyState === "complete") {
+    init();
+  } else {
+    addEventListener("load", init);
+  }
+}
 
 const GoogleTagManager = ({ trackingId = "" }: Props) => (
   <>
     <Script
+      id={`gtag-script-${trackingId}`}
       forward={["dataLayer.push"]}
       src={`https://www.googletagmanager.com/gtag/js?id=${trackingId}`}
     />
     <Script
-      type="application/javascript"
+      id={`gtag-script-global-${trackingId}`}
+      type="module"
       dangerouslySetInnerHTML={{
-        __html: snippet(trackingId),
+        __html: `(${snippet})(${trackingId});`,
       }}
     />
   </>
