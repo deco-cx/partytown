0d3f3c7455771fce22883c3b3384290e5d2ba8e8
Igor Brasileiro <brasileiro456@gmail.com>
2023-10-02 13:45:33 -0300

    fix: prevent forward when `dangerouslyRunOnMainThread` is true (#13)
    * Script handle when should collect properties

--- Script.tsx
+++ Script.tsx
@@ -2,7 +2,12 @@ import { Head } from "$fresh/runtime.ts";
 import { JSX } from "preact";
 import { collectForward } from "./shared.ts";
 
+const TYPE_TEXT_PARTYTOWN = "text/partytown";
+
 interface Props extends JSX.HTMLAttributes<HTMLScriptElement> {
+  /**
+   * add all strings at Partytown forward config, if type is `undefined` or `text/partytown`
+   */
   forward?: string[];
 }
 
@@ -10,11 +15,16 @@ const Script = ({
   forward = [],
   ...scriptProps
 }: Props) => {
-  collectForward(forward);
+  const shouldCollect = scriptProps.type === undefined ||
+    scriptProps.type === TYPE_TEXT_PARTYTOWN;
+
+  if (shouldCollect) {
+    collectForward(forward);
+  }
 
   return (
     <Head>
-      <script type="text/partytown" {...scriptProps} />
+      <script type={TYPE_TEXT_PARTYTOWN} {...scriptProps} />
     </Head>
   );
 };
